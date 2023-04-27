1d70298d967a86992133cbdc1838b584fe4985b7
Michael Schmid <schmid.m9@googlemail.com>
2023-04-27 07:53:55 +0200

    comments, packinglist and itinerary to trip, style

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,52 @@
+// Switch Button Landingpage
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container");
+
+btn.addEventListener("click", function () {
+    if (!btn.classList.contains("slide")){
+        btn.classList.add("slide");
+        video.pause();
+    } else {
+        btn.classList.remove("slide");
+        video.play();
+    }
+});
+
+// preloader
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", function () {
+    preloader.classList.add("hide-preloader");
+})
+
+// Back to top Button
+// Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 50 ||
+    document.documentElement.scrollTop > 50
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 // Glow Effect Mouse/Pointer
 // document.addEventListener("mousemove", function(event) {
 //   var x = event.clientX;
@@ -47,50 +96,3 @@
 // document.addEventListener('mouseout', () => {
 //   glowPointer.style.opacity = 0;
 // });
-
-const btn = document.querySelector(".switch-btn");
-const video = document.querySelector(".video-container");
-
-btn.addEventListener("click", function () {
-    if (!btn.classList.contains("slide")){
-        btn.classList.add("slide");
-        video.pause();
-    } else {
-        btn.classList.remove("slide");
-        video.play();
-    }
-});
-
-// preloader
-const preloader = document.querySelector(".preloader");
-
-window.addEventListener("load", function () {
-    preloader.classList.add("hide-preloader");
-})
-// Back to top Button
-// Get the button
-let mybutton = document.getElementById("btn-back-to-top");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 50 ||
-    document.documentElement.scrollTop > 50
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
-
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
